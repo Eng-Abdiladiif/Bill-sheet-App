478c8252f0ba22b95511320e958bea8d181604cf
cabdiladiif <cladiifsaalax@gmail.com>
2024-02-27 10:51:47 +0300

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`{inter.className}
+   bg-[#172554]`}>{children}</body>
     </html>
   );
 }
